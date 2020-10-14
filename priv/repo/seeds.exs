69fbe09cc5f5cd78977f29abd6062cbf7f881135
herulume <herulume@protonmail.com>
2020-10-14 22:39:03 +0100

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Infin.Accounts.User
+alias Infin.Repo
+
+Repo.insert!(
+  %User{}
+  |> User.registration_changeset(%{email: "test@mail.com", password: "qwerty1234567891"})
+)
+
+Repo.insert!(
+  %User{}
+  |> User.registration_changeset(%{email: "test2@mail.com", password: "qwerty1234567892"})
+)
