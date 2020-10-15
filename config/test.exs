a2009ade31a7ed73cd7ab095fc0a9652a1fa2b4b
João Vilaça <machadovilaca@gmail.com>
2020-10-15 01:27:42 +0100

    Change test postgres credentials

--- config/test.exs
+++ config/test.exs
@@ -15,13 +15,6 @@ config :infin, Infin.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# Configure the database for GitHub Actions
-if System.get_env("GITHUB_ACTIONS") do
-  config :app, App.Repo,
-    username: "postgres",
-    password: "postgres"
-end
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :infin, InfinWeb.Endpoint,
