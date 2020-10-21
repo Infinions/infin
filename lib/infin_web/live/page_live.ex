f05290544718e5405dfa228d0dbfec398006e047
João Vilaça <machadovilaca@gmail.com>
2020-10-21 12:56:21 +0100

    Add impl to live functions

--- lib/infin_web/live/page_live.ex
+++ lib/infin_web/live/page_live.ex
@@ -1,17 +1,20 @@
 defmodule InfinWeb.PageLive do
   use InfinWeb, :live_view
 
+  @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: Process.send_after(self(), :update, 1000)
 
     {:ok, assign(socket, time: DateTime.utc_now)}
   end
 
+  @impl true
   def handle_info(:update, socket) do
     Process.send_after(self(), :update, 1000)
     {:noreply, assign(socket, time: DateTime.utc_now)}
   end
 
+  @impl true
   def handle_event("update", _value, socket) do
     {:noreply, assign(socket, time: DateTime.utc_now)}
   end
