baa48ec4b5d7d7d8b09519f34f62b7096999d55a
herulume <herulume@protonmail.com>
2020-10-28 15:10:14 +0000

    Break tests

--- lib/infin/accounts/user.ex
+++ lib/infin/accounts/user.ex
@@ -30,32 +30,6 @@ defmodule Infin.Accounts.User do
     |> validate_password()
   end
 
-  defp to_company_form(%{"nif" => nif, "name" => name} = attrs),
-    do: Map.put(attrs, "company", %{"nif" => nif, "name" => name})
-
-  defp to_company_form(%{nif: nif, name: name} = attrs),
-    do: Map.put(attrs, :company, %{nif: nif, name: name})
-
-  defp to_company_form(%{} = attrs), do: attrs
-
-  def copy_company_errors(%Ecto.Changeset{} = changeset) do
-    cond do
-      Map.has_key?(changeset.changes, :company) ->
-        company = changeset.changes.company
-
-        Enum.reduce(company.errors, changeset, fn err, acc ->
-          key = Kernel.elem(err, 0)
-          pre_message = Kernel.elem(err, 1)
-          message = Kernel.elem(pre_message, 0)
-
-          Ecto.Changeset.add_error(acc, key, message)
-        end)
-
-      true ->
-        changeset
-    end
-  end
-
   defp validate_email(changeset) do
     changeset
     |> validate_required([:email])
