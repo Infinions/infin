a9f38db8df23fb5715da02b62f7264bc7ffa6b4c
BrunoMartins11 <brunocmartins@live.com.pt>
2020-10-31 18:35:21 +0000

    Revert "Add market context and enterprises  (#16)" (#18)

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,12 +2,12 @@ defmodule Infin.Factory do
   use ExMachina.Ecto, repo: Infin.Repo
 
   alias Infin.Companies.{Company, Category}
-  alias Infin.Market.Enterprise
 
   def company_factory do
     %Company{
       name: sequence("Company"),
       nif: sequence("123456789")
+
     }
   end
 
@@ -15,13 +15,6 @@ defmodule Infin.Factory do
     %Category{
       name: sequence("Category"),
       company_id: build(:company)
-    }
-  end
-
-  def enterprise_factory do
-    %Enterprise{
-      name: sequence("Enterprise"),
-      nif: sequence("123456789")
     }
   end
 end
