64723d2bacf2145f7a0b8ffbbcf19bb17eb50345
BrunoMartins11 <brunocmartins@live.com.pt>
2020-11-03 15:42:17 +0000

    Remove timestamp

--- priv/repo/migrations/20201103151848_create_invoices_tags.exs
+++ priv/repo/migrations/20201103151848_create_invoices_tags.exs
@@ -5,8 +5,6 @@ defmodule Infin.Repo.Migrations.CreateInvoicesTags do
     create table(:invoices_tags) do
       add :invoice_id, references(:invoices)
       add :tag_id, references(:tags)
-
-      timestamps()
     end
 
     create unique_index(:invoices_tag, [:invoice_id, :tag_id])
