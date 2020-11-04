f97be56b5f51b14a3c67190789c79ed83265bf1e
BrunoMartins11 <brunocmartins@live.com.pt>
2020-11-04 15:50:33 +0000

    initial dev invoices

--- lib/infin/invoices/invoice.ex
+++ lib/infin/invoices/invoice.ex
@@ -5,6 +5,34 @@ defmodule Infin.Invoices.Invoice do
 
   schema "invoices" do
     field :id_document, :string
+    field :registration_orig, :string
+    field :registration_orig_desc, :string
+    field :doc_type, :string
+    field :doc_type_dec, :string
+    field :doc_number, :string
+    field :doc_hash, :string
+    field :doc_emition_date, :string
+    field :total_value, :integer
+    field :total_base_value, :integer
+    field :total_tax_value, :integer
+    field :total_benef_prov_value, :integer
+    field :total_benef_sector_value, :integer
+    field :total_gen_exp_value, :integer
+    field :benef_state, :string
+    field :benef_state_desc, :string
+    field :benef_state_emit, :string
+    field :benef_state_emit_desc, :string
+    field :normal_tax_exists, :boolean
+    field :emit_activity, :string
+    field :emit_activity_desc, :string
+    field :prof_activity, :string
+    field :prof_activity_desc, :string
+    field :merchant_comm, :boolean
+    field :consumer_comm, :boolean
+    field :is_foreign, :boolean
+
+    belongs_to :company, Infin.Companies.Company
+
 
     many_to_many :tags, Tag, join_through: "invoices_tags"
     timestamps()
