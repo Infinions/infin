2e4f58414363284a4a974bb6ae12e1ea5396c41a
João Vilaça <machadovilaca@gmail.com>
2020-11-14 18:25:41 +0000

    Improve footer and fix nav-burger (#34)
    * Improve footer
    
    * Fix nav-burger click
    
    * Remove console.log

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -11,7 +11,7 @@ $(window).on("scroll", () => {
   }
 });
 
-$(document).on("ready", () => {
+$(() => {
   $(".navbar-burger").on("click", () => {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
