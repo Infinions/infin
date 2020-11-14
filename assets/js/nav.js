0f4735231709b942ff454badd40d83fad14ee140
João Vilaça <machadovilaca@gmail.com>
2020-11-14 01:56:32 +0000

    Fix nav-burger click

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -11,7 +11,9 @@ $(window).on("scroll", () => {
   }
 });
 
-$(document).on("ready", () => {
+$(() => {
+  console.log('ola');
+
   $(".navbar-burger").on("click", () => {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
