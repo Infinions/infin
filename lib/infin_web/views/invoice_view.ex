fe39eec87f62366488caf70dac661f0bc126558c
herulume <herulume@protonmail.com>
2020-11-30 17:19:43 +0000

    Add tags_to_string aux

--- lib/infin_web/views/invoice_view.ex
+++ lib/infin_web/views/invoice_view.ex
@@ -1,7 +1,21 @@
 defmodule InfinWeb.InvoiceView do
   use InfinWeb, :view
 
+  alias Infin.Invoices.Tag
+
   def contains_invoice?(assigns) do
     assigns[:invoice]
+  end
+
+  def tags_to_string([%Tag{}] = tags) do
+    Enum.reduce(tags, "", fn tag, acc ->
+      case acc do
+        "" ->
+          tag.name
+
+        _ ->
+          tag.name <> ", " <> acc
+      end
+    end)
   end
 end
