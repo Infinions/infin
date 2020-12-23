36856ce14fe614c566b98807831eb902e61b251a
Jéssica Lemos <32941964+jessicalemos@users.noreply.github.com>
2020-12-23 17:45:07 +0000

    Update attach pdf CSS (#65)

---
+++ assets/js/file.js
@@ -0,0 +1,8 @@
+const fileInput = document.querySelector('#file input[type=file]');
+
+fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+        const fileName = document.querySelector('#file .file-name');
+        fileName.textContent = fileInput.files[0].name;
+    }
+}
